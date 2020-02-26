6e9beae5aa820d93e6f4e12a25aafa203edef442
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-02-26 15:05:16 -0800

    add @initbinder for pre-preocessing

--- spring-mvc-demo/src/com/dafe/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/com/dafe/springdemo/mvc/CustomerController.java
@@ -4,9 +4,12 @@ package com.dafe.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +17,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customer")
 public class CustomerController {
 
+	//add @initbinder ... to convert trim to input strings
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+	
+	dataBinder.registerCustomEditor (String.class, stringTrimmerEditor);
+	}
+	
+	//remove leading and trailing whitespaces
+	
+	
+	
+	//resolve issue for our validation
+	
+	
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
@@ -30,6 +49,7 @@ public class CustomerController {
 		@Valid @ModelAttribute("customer") Customer theCustomer, 
 		BindingResult theBindingResult) {
 		
+		System.out.println("Last Name: |" + theCustomer.getLastName() + "|");
 		if(theBindingResult.hasErrors()) {
 		return "customer-form";
 		} 
