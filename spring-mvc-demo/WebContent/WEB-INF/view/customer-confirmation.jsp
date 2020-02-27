271a480d4f4203197fe435d981c1543ed6a02528
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-02-26 18:00:24 -0800

    applying regex in spring mvc

--- spring-mvc-demo/WebContent/WEB-INF/view/customer-confirmation.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/customer-confirmation.jsp
@@ -15,6 +15,12 @@ The Customer is confirmed: ${customer.firstName } ${customer.lastName }
 
 Free Passes: ${customer.freePasses}
 
+<br><br>
+
+Postal Code : ${customer.postalCode}
+
+
+
 </body>
 
 </html>
--- spring-mvc-demo/src/com/dafe/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/dafe/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.dafe.springdemo.mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -12,9 +13,13 @@ public class Customer {
 	@NotNull(message="is Required")
 	@Size(min=1, message="is Required")
 		private String lastName;
+	
 	@Min(value=0, message="must be greater than or equal to 0")
 	@Max(value=10, message="must be less than or equal to 10")
 	private int freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 characters or digits")
+	private String postalCode;
 	
 	
 	public String getFirstName() {
@@ -35,6 +40,12 @@ public class Customer {
 	}
 	public void setFreePasses(int freePasses) {
 		this.freePasses = freePasses;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 	
 	
