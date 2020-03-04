be98c15118c15a65df06908cb5f83b3ef7e4edfb
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-03-03 17:56:09 -0800

    unidirectional @onetomany create course reviews

---
+++ hb-04-one-one-many-uni/src/com/dafe/hibernate/demo/CreateCourseAndReviewsDemo.java
@@ -0,0 +1,66 @@
+package com.dafe.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.dafe.hibernate.demo.entity.Course;
+import com.dafe.hibernate.demo.entity.Instructor;
+import com.dafe.hibernate.demo.entity.InstructorDetail;
+import com.dafe.hibernate.demo.entity.Review;
+
+public class CreateCourseAndReviewsDemo {
+
+	public static void main(String[] args) {
+
+		//create session factory
+		SessionFactory factory = new Configuration().
+				configure("hibernate.cfg.xml").
+				addAnnotatedClass(Instructor.class).
+				addAnnotatedClass(InstructorDetail.class).
+				addAnnotatedClass(Course.class).
+				addAnnotatedClass(Review.class).
+				buildSessionFactory();
+		
+		//create session
+
+	Session session = factory.getCurrentSession();
+	
+	try {
+		
+		
+		//start a transaction
+		session.beginTransaction();
+		
+		//create a course
+		
+		Course tempCourse = new Course("Java For Beginners");
+		
+		//add some reviews
+		tempCourse.addReviews(new Review("Great course, recommend it for newbies!"));
+		tempCourse.addReviews(new Review("Fantastic course, you deserve 5 stars!"));
+		tempCourse.addReviews(new Review("What a stupid course!"));
+
+		
+		//save the course and leverage the cascade all
+		System.out.println("Saving the course!!");
+		System.out.println(tempCourse);
+		System.out.println(tempCourse.getReviews());
+		
+		session.save(tempCourse);
+		
+		//commit transaction
+		session.getTransaction().commit();
+		System.out.println("Done!!");
+		
+	}finally{
+		
+		
+		//add clean up code
+		session.close();
+		factory.close();
+	}
+	
+	}
+
+}
--- hb-04-one-one-many-uni/src/com/dafe/hibernate/demo/entity/Course.java
+++ hb-04-one-one-many-uni/src/com/dafe/hibernate/demo/entity/Course.java
@@ -1,13 +1,18 @@
 package com.dafe.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -31,13 +36,14 @@ public class Course {
 	@JoinColumn(name="instructor_id")
 	private Instructor instructor;
 	
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="course_id")
+	private List <Review> reviews;
+	
 	//create constructor
 	public Course () {
-		
-		
 	}
-
-	
 	//generate getters and setters
 
 	public int getId() {
@@ -64,6 +70,24 @@ public class Course {
 		this.instructor = instructor;
 	}
 
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	//add convenience method to add review to the list
+	public void addReviews(Review theReview) {
+		
+		if(reviews==null) {
+			reviews = new ArrayList<>();
+		
+			reviews.add(theReview);
+		}
+	}
 	//generate constructor
 	
 	public Course(String title) {
