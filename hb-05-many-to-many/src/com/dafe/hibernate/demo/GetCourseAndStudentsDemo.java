f8b32cc7a8e1dbf528dd6d5a89983f3385b14577
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-03-04 19:21:30 -0800

    get courses for students

---
+++ hb-05-many-to-many/src/com/dafe/hibernate/demo/GetCourseAndStudentsDemo.java
@@ -0,0 +1,62 @@
+package com.dafe.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.dafe.hibernate.demo.entity.Course;
+import com.dafe.hibernate.demo.entity.Instructor;
+import com.dafe.hibernate.demo.entity.InstructorDetail;
+import com.dafe.hibernate.demo.entity.Review;
+import com.dafe.hibernate.demo.entity.Student;
+
+public class GetCourseAndStudentsDemo {
+
+	public static void main(String[] args) {
+
+		//create session factory
+		SessionFactory factory = new Configuration().
+				configure("hibernate.cfg.xml").
+				addAnnotatedClass(Instructor.class).
+				addAnnotatedClass(InstructorDetail.class).
+				addAnnotatedClass(Course.class).
+				addAnnotatedClass(Review.class).
+				addAnnotatedClass(Student.class).
+						buildSessionFactory();
+		
+		//create session
+
+	Session session = factory.getCurrentSession();
+	
+	try {
+		
+		
+		//start a transaction
+		session.beginTransaction();
+		
+		//get the student John from the db
+int studentId = 1;		
+Student tempStudent = session.get(Student.class, studentId);		
+
+System.out.println("\nLoaded students...!!" + tempStudent);
+System.out.println("\nCourses" + tempStudent.getCourses() );
+
+
+
+
+
+		//commit transaction
+		session.getTransaction().commit();
+		System.out.println("Done!!");
+		
+	}finally{
+		
+		
+		//add clean up code
+		session.close();
+		factory.close();
+	}
+	
+	}
+
+}
