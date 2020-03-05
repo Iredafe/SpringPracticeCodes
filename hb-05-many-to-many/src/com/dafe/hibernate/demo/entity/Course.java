faa04fbce6a29dd4b08069c6026f63666fc6caa6
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-03-04 16:41:09 -0800

    update student class for many to many mapping

--- hb-05-many-to-many/src/com/dafe/hibernate/demo/entity/Course.java
+++ hb-05-many-to-many/src/com/dafe/hibernate/demo/entity/Course.java
@@ -4,19 +4,21 @@
 	package com.dafe.hibernate.demo.entity;
 
 	import java.util.ArrayList;
-	import java.util.List;
+import java.util.List;
 
-	import javax.persistence.CascadeType;
-	import javax.persistence.Column;
-	import javax.persistence.Entity;
-	import javax.persistence.FetchType;
-	import javax.persistence.GeneratedValue;
-	import javax.persistence.GenerationType;
-	import javax.persistence.Id;
-	import javax.persistence.JoinColumn;
-	import javax.persistence.ManyToOne;
-	import javax.persistence.OneToMany;
-	import javax.persistence.Table;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 	@Entity
 	@Table(name="course")
@@ -49,7 +51,13 @@
 		@JoinColumn(name="course_id")
 		private List<Review> reviews;
 			
-		
+		@ManyToMany(fetch=FetchType.LAZY, cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+				 CascadeType.DETACH, CascadeType.REFRESH})	
+		@JoinTable(
+				name="course_id",
+				joinColumns=@JoinColumn(name="course_id"),
+				inverseJoinColumns=@JoinColumn(name="student_id")
+				)
 		private List <Student>  students;
 		
 		public Course() {
